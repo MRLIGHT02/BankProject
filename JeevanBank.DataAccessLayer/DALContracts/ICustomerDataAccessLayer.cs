9a8c83955c6b514d1d4d4f8b734454d8faa0e38e
MRLIGHT02 <saifali42020@gmail.com>
2025-05-23 15:08:50 +0530

    #

---
+++ JeevanBank.DataAccessLayer/DALContracts/ICustomerDataAccessLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeevanBank.DataAccessLayer.DALContracts
+{
+    /// <summary>
+    /// Defines methods for accessing and managing customer data in a data store.
+    /// </summary>
+    /// <remarks>This interface provides an abstraction for customer data operations, allowing implementations
+    /// to interact with various data storage mechanisms (e.g., databases, in-memory collections).</remarks>
+    internal interface ICustomerDataAccessLayer
+    {
+    }
+}
