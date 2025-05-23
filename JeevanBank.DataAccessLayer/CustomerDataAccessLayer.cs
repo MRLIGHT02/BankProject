48474790633e26b6cc8c40273d46355479e60eee
MRLIGHT02 <saifali42020@gmail.com>
2025-05-23 15:20:33 +0530

    Remove Class1 and add CustomerDataAccessLayer
    This commit removes the `Class1.cs` file and its references. A new class, `CustomerDataAccessLayer`, has been introduced to handle customer data access. Additionally, the `ICustomerDataAccessLayer` interface has been updated to include new methods for updating and deleting customers, improving customer management functionality.

--- JeevanBank.DataAccessLayer/CustomerDataAccessLayer.cs
+++ JeevanBank.DataAccessLayer/CustomerDataAccessLayer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace JeevanBank.DataAccessLayer
 {
-    public class Class1
+    public class CustomerDataAccessLayer
     {
     }
 }
--- JeevanBank.DataAccessLayer/DALContracts/ICustomerDataAccessLayer.cs
+++ JeevanBank.DataAccessLayer/DALContracts/ICustomerDataAccessLayer.cs
@@ -32,9 +32,21 @@ namespace JeevanBank.DataAccessLayer.DALContracts
         /// <param name="customer">The customer to add. Must not be null and must contain valid data.</param>
         /// <returns>The unique identifier (<see cref="Guid"/>) assigned to the newly added customer.</returns>
         Guid AddCustomer(Customer customer);
-
+        /// <summary>
+        /// Updates the details of an existing customer in the system.
+        /// </summary>
+        /// <param name="customer">The customer object containing updated information. The <see cref="Customer.Id"/> property must match an
+        /// existing customer.</param>
+        /// <returns><see langword="true"/> if the customer was successfully updated; otherwise, <see langword="false"/>.</returns>
         bool UpdateCustomer(Customer customer);
-
+        /// <summary>
+        /// Deletes the customer with the specified unique identifier.
+        /// </summary>
+        /// <remarks>Use this method to remove a customer from the system. Ensure that the provided
+        /// <paramref name="CustomerID"/> corresponds to an existing customer. If the customer does not exist, the
+        /// method will return <see langword="false"/>.</remarks>
+        /// <param name="CustomerID">The unique identifier of the customer to delete.</param>
+        /// <returns><see langword="true"/> if the customer was successfully deleted; otherwise, <see langword="false"/>.</returns>
         bool DeleteCustomer(Guid CustomerID);
 
     }
