39e781924e318b2589073e995f306aab5f7baf7e
MRLIGHT02 <saifali42020@gmail.com>
2025-05-23 15:24:39 +0530

    Refactor CustomerDataAccessLayer with new methods
    Added using directives for necessary namespaces and removed unused ones.
    Included a summary comment for the CustomerDataAccessLayer class.
    Implemented the ICustomerDataAccessLayer interface with method stubs for customer operations, each throwing NotImplementedException.

--- JeevanBank.DataAccessLayer/CustomerDataAccessLayer.cs
+++ JeevanBank.DataAccessLayer/CustomerDataAccessLayer.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using JeevanBank.Entities;
+using JeevanBank.Exceptions;
+using JeevanBank.DataAccessLayer.DALContracts;
 
 namespace JeevanBank.DataAccessLayer
 {
-    public class CustomerDataAccessLayer
+    /// <summary>
+    /// Provides methods for accessing and managing customer data in the underlying data store.
+    /// </summary>
+    /// <remarks>This class serves as a data access layer (DAL) for customer-related operations.  It is
+    /// responsible for interacting with the data source to retrieve, update, and manage customer information.</remarks>
+    public class CustomerDataAccessLayer : ICustomerDataAccessLayer
     {
+        public Guid AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCustomer(Guid CustomerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomersByCondition(Predicate<Customer> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
