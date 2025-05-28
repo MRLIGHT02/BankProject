7e4ce83599181f7511a10d93299c8346bad4fc01
MRLIGHT02 <saifali42020@gmail.com>
2025-05-28 12:19:00 +0530

    #

---
+++ JeevanBank.BusinessLogicLayer/BALContracts/ICustomerBusinessLogicLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeevanBank.BusinessLogicLayer.BALContracts
+{
+    internal class ICustomerBusinessLogicLayer
+    {
+    }
+}
