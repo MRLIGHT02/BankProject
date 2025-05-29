83ed8ad1479b3b26ad6192edd8b087fa13bd221a
MRLIGHT02 <saifali42020@gmail.com>
2025-05-29 13:14:11 +0530

    #

--- BankProject/CustomerPresentation.cs
+++ BankProject/CustomerPresentation.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using JeevanBank.Entities;
+using JeevanBank.BusinessLogicLayer;
+using JeevanBank.Exceptions;
 namespace BankProject
 {
-    internal class CustomerPresentation
+    static class CustomerPresentation
     {
     }
 }
